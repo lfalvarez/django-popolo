f505e9a68ddb4e442d0cbacf50d6616b937d40dc
Gabriele Lucci <gabriele@lucci.xyz>
2018-12-06 20:16:59 +0100

    setuptools: Pull django-autoslug from github
    Current published version of django-autoslug doesn't support Django 2

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+
 from setuptools import setup, find_packages
 
 
@@ -9,43 +10,38 @@ def read_file(filename):
     try:
         return open(filepath).read()
     except IOError:
-        return ''
+        return ""
 
 
 setup(
-    name='django-popolo',
-    version=__import__('popolo').__version__,
-    author='Guglielmo Celata',
-    author_email='guglielmo@openpolis.it',
+    name="django-popolo",
+    version=__import__("popolo").__version__,
+    author="Guglielmo Celata",
+    author_email="guglielmo@openpolis.it",
     packages=find_packages(),
     include_package_data=True,
-    url='http://github.com/openpolis/django-popolo',
-    license='Affero',
-    description=u' '.join(__import__('popolo').__doc__.splitlines()).strip(),
+    url="http://github.com/openpolis/django-popolo",
+    license="Affero",
+    description=u" ".join(__import__("popolo").__doc__.splitlines()).strip(),
     classifiers=[
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Framework :: Django',
-        'Development Status :: 5 - Production/Stable',
-        'Operating System :: OS Independent',
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Django",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
     ],
-    long_description=read_file('README.rst'),
+    long_description=read_file("README.rst"),
     test_suite="runtests.runtests",
     zip_safe=False,
-    tests_require=[
-        'Django==1.11',
-        'Faker==0.8.13',
-        'factory_boy==2.10.0',
-        'mock==2.0.0'
-    ],
-    install_requires=[
-        "django-autoslug",
-        "django-model-utils",
+    tests_require=["Django", "fake-factory", "mock==2.0.0"],
+    install_requires=["django-autoslug==1.9.4-dev", "django-model-utils"],
+    dependency_links=[
+        "git+ssh://git@github.com/justinmayer/django-autoslug.git" "@4dc7508#egg=django_autoslug-1.9.4_dev"
     ],
 )
